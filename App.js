1bfa8d38f883e1c9ae3719d9eb75778d6ed54ebd
Jesse Biroscak <hackajesse@users.noreply.github.com>
2024-12-17 15:16:56 -0500

    created app.js and added initial nav

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/HomeScreen'; // Adjust the path if necessary
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                {/* Add other screens here as you create them */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
