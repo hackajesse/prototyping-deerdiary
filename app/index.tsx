1e521e63624e17f48c82bb16c9a8ed358a7ad341
Jesse Biroscak <hackajesse@users.noreply.github.com>
2024-12-17 18:17:11 -0500

    updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen'; // Adjust the path if necessary
+import HomeScreen from '../src/screens/HomeScreen'; // Adjust the path if necessary
 
 const Stack = createNativeStackNavigator();
 
